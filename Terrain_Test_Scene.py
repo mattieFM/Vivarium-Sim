d2ef1971742b36fd01e2c9014fbd3dd94fd2d088
mattiefm <mmfuller22@gmail.com>
2024-11-22 03:08:56 -0700

    brush radius, brush strength edit toggle

--- Terrain_Test_Scene.py
+++ Terrain_Test_Scene.py
@@ -1,4 +1,6 @@
 from direct.showbase.ShowBase import ShowBase
+from direct.showbase.DirectObject import DirectObject
+from direct.gui.DirectGui import *
 from panda3d.core import *
 from direct.task import Task
 from direct.actor.Actor import Actor
@@ -7,7 +9,155 @@ from math import pi, sin, cos
 import math
 from util import Util
 
-class Input():
+MIN_UI_X =-1.25
+MAX_UI_X =1.25
+
+MIN_UI_Y = .9
+MAX_UI_Y = -.9
+
+UI_Y_WIDTH = np.abs(MIN_UI_Y)+np.abs(MAX_UI_Y)
+UI_X_WIDTH = np.abs(MIN_UI_X)+np.abs(MAX_UI_X)
+
+
+class ConfigurableValue():
+    """a class for values that can be configured via the UI"""
+    def __init__(
+        self,
+        callback,
+        label,
+        is_toggle,
+        scale = .05,
+        y_spacing_between_elements=UI_Y_WIDTH/30,
+        x_spacing_between_elements=UI_Y_WIDTH/100,
+        start_x=0,
+        start_y=0,
+        placeholder=""
+        ):
+        self.callback = callback
+        self.label=label
+        self.is_toggle = is_toggle
+        self.scale = scale
+        self.y_spacing_between_elements = y_spacing_between_elements
+        self.x_spacing_between_elements = x_spacing_between_elements
+        self.elements = []
+        self.placeholder=placeholder
+        
+        self.start_x=start_x
+        self.start_y=start_y
+        
+    def destroy(self):
+        """Destroy this element"""
+        for element in self.elements:
+            element.destroy()
+    
+    def update(self):
+        "Destroy then redraw this element"
+        self.destroy()
+        self.create_ui_component(self.start_x,self.start_y)
+        
+    def unfocus(self):
+        """unfocus any and all elements that could be focused"""
+        for element in self.elements:
+            #TODO:clean this up.
+            try:
+                element['focus'] = False
+            except Exception:
+                pass
+    
+    def create_ui_component(self,start_x,start_y):
+        self.start_x=start_x
+        self.start_y=start_y
+        
+        self.element = None
+        x=start_x
+        y=start_y
+        
+        self.labelElement = DirectLabel(text=self.label,scale=self.scale*1.4,pos=(MIN_UI_X+x,0,MIN_UI_Y+y-.015))
+        
+        label_width = self.labelElement.getWidth()  * self.labelElement['scale']
+        
+        label_x = MIN_UI_X+x+label_width*.5
+        
+        self.labelElement.setPos((label_x,0,MIN_UI_Y+y-.015))
+        
+        print(f"labelX{label_x},label_width:{label_width}")
+        x = -MIN_UI_X + label_x + label_width*.5 + self.x_spacing_between_elements + .05
+        print(x)
+        
+        self.elements.append(self.labelElement)
+        
+        if(self.is_toggle):
+            #then render as just a toggle button and label
+            self.element = DirectCheckButton(text = "" ,scale=self.scale,command=self.callback,pos=(MIN_UI_X+x,0,MIN_UI_Y+y))
+            self.elements.append(self.element)
+            pass
+        else:
+            #else render as a label input box and set button
+            self.textBox = DirectEntry(scale=self.scale-.0125,pos=(MIN_UI_X+x-.039,0,MIN_UI_Y+y-.01),command=self.callback,initialText=str(self.placeholder))
+            self.elements.append(self.textBox)
+            text_box_width = self.textBox.getWidth()  * self.textBox['scale']
+            
+            x += text_box_width + self.x_spacing_between_elements
+            
+            def call_cb_with_text_from_textbox():
+                """call the callback passing in text"""
+                self.callback(self.textBox.get())
+                self.textBox["focus"] = 0
+            
+            self.element = DirectButton(text = "Set", scale=self.scale+.0025,command=call_cb_with_text_from_textbox,pos=(MIN_UI_X+x,0,MIN_UI_Y+y-.011))
+            self.elements.append(self.element)
+            pass
+        
+        return self.element
+        
+
+class UI():
+    VERTICAL_SPACING_BETWEEN_OPTIONS = UI_Y_WIDTH/25
+    
+    def __init__(self):
+        self.configurable_values = []
+        self.add_base_options()
+        
+    def add_base_options(self):
+        """add the base options to this UI instance
+        """
+        #self.configurable_values.append(ConfigurableValue(lambda x: 0, "test", True))
+        #self.configurable_values.append(ConfigurableValue(lambda x: print(x), "test", False))
+        pass
+        
+    def add_option(self,option):
+        """add a configurable value to the UI and update and render it
+
+        Args:
+            option (ConfigurableValue): the option to add
+        """
+        self.configurable_values.append(option)
+        self.update()
+        print(self.configurable_values)
+
+    def update(self):
+        """destroy all displayed config options then redraw all of them"""
+        for value in self.configurable_values:
+            value.destroy()
+        
+        self.setup()
+        
+    def unfocus_all(self):
+        """unfocus all elements that can be unfocused"""
+        for value in self.configurable_values:
+            value.unfocus()
+    
+    def setup(self):
+        """render all elements in a list
+        """
+        y=0
+        print("setup")
+        for value in self.configurable_values:
+            value.create_ui_component(0,y)
+            y-=self.VERTICAL_SPACING_BETWEEN_OPTIONS
+    
+
+class Input(DirectObject):
     VERTICAL_SPEED=1
     HORIZONTAL_SPEED=.8
     ZOOM_SPEED=30
@@ -45,14 +195,14 @@ class Input():
         self.base = base
         
         # Setup event listeners for the mouse wheel
-        self.base.accept('wheel_up', self.on_mouse_scroll, ['wheel_up'])
-        self.base.accept('wheel_down', self.on_mouse_scroll, ['wheel_down'])
+        self.accept('wheel_up', self.on_mouse_scroll, ['wheel_up'])
+        self.accept('wheel_down', self.on_mouse_scroll, ['wheel_down'])
         
-        self.base.accept("mouse1", self.on_mouse_click, ['mouse1'])
-        self.base.accept("mouse1-up", self.on_mouse_click, ['mouse1-up'])
+        self.accept("mouse1", self.on_mouse_click, ['mouse1'])
+        self.accept("mouse1-up", self.on_mouse_click, ['mouse1-up'])
         
-        self.base.accept("mouse3", self.on_mouse3_click, ['mouse3'])
-        self.base.accept("mouse3-up", self.on_mouse3_click, ['mouse3-up'])
+        self.accept("mouse3", self.on_mouse3_click, ['mouse3'])
+        self.accept("mouse3-up", self.on_mouse3_click, ['mouse3-up'])
         
     def on_mouse_click(self,event):
         print(f"event:{event}")
@@ -201,7 +351,13 @@ class CameraController():
 
 class BaseApp(ShowBase):     
     #so that it can be turned off if we want to
-    edit_terrain_enabled = True
+    edit_terrain_enabled = False
+    
+    #how fast to build terrain
+    edit_power = .5
+    
+    #how big to edit 
+    edit_radius = 50
       
     def __init__(self):
         ShowBase.__init__(self)
@@ -211,6 +367,26 @@ class BaseApp(ShowBase):
         
         #init our input handler class
         self.input = Input(self)
+        
+        self.ui = UI()
+        self.ui.setup()
+        
+        def edit_terrain_toggle(val):
+            self.edit_terrain_enabled=val
+            
+        def edit_speed(val):
+            self.edit_power=float(val)
+            self.ui.unfocus_all
+            
+        def edit_radius(val):
+            self.edit_radius=float(val)
+            self.ui.unfocus_all
+            
+        self.ui.add_option(ConfigurableValue(edit_terrain_toggle, "edit", True))
+        
+        self.ui.add_option(ConfigurableValue(edit_speed, "edit speed", False, placeholder=self.edit_power))
+        
+        self.ui.add_option(ConfigurableValue(edit_radius, "edit radius", False, placeholder=self.edit_radius))
         
         #--terrain setup--
         self.terrain = GeoMipTerrain("worldTerrain")
@@ -230,7 +406,7 @@ class BaseApp(ShowBase):
         self.render.setLight(self.alight)
         
         self.render.setShaderAuto()
-        
+            
         # Load and compile the shader
         #self.shader = Shader.load(Shader.SL_GLSL, "./assets/shaders/shadow_shader.vert", "./assets/shaders/shadow_shader.frag")
         
@@ -267,12 +443,8 @@ class BaseApp(ShowBase):
         self.picker = CollisionTraverser()
         self.queue = CollisionHandlerQueue()
         
-        self.pandaActor = Actor("models/panda-model",
-                                {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.3, 0.3, 0.3)
-        self.pandaActor.reparentTo(self.render)
         
-        
+        #setup selection/picker handlers so we can click on objects and do things
         self.picker_node = CollisionNode('mouseRay')
         self.picker_np = self.cam.attach_new_node(self.picker_node)
         self.picker_node.setFromCollideMask(GeomNode.getDefaultCollideMask())
@@ -282,6 +454,12 @@ class BaseApp(ShowBase):
         
         self.taskMgr.add(self.updateTask, "update")
         self.task_mgr.add(self.handle_terrain_edit, "handleEnvironmentChange")
+        
+        #handles unfocusing from inputs when user clicks out of them
+        #self.accept("mouse1-up", self.ui.unfocus_all, ['mouse1-up'])
+        
+        self.task_mgr.add(self.handle_unfocus, "handle_unfocus")
+        
         
         #init our camera controller
         self.camera_controller = CameraController(
@@ -323,8 +501,14 @@ class BaseApp(ShowBase):
                 # Find the collision point
                 point = entry.getSurfacePoint(self.terrain_np)
                 self.raise_point(point,power=modifier)
+                
+    def handle_unfocus(self,task):
+        if(self.input.mouse_held or self.input.mouse3_held):
+            self.ui.unfocus_all()
+        return Task.cont
         
-    def handle_terrain_edit(self, task, modifier=.5):
+    def handle_terrain_edit(self, task, modifier=None):
+        if(modifier == None): modifier = self.edit_power
         if(self.edit_terrain_enabled):
             if(self.input.mouse_held):
                 self.edit_terrain(modifier)
@@ -333,7 +517,8 @@ class BaseApp(ShowBase):
             
         return Task.cont
                 
-    def raise_point(self, point, max_range=50,power=.1):
+    def raise_point(self, point, max_range=None,power=.1):
+        if(max_range == None): max_range = self.edit_radius
         # Convert the world point to heightmap coordinates
         print(f"pointx:{point.x}, pointy:{point.y}")
         center_x = point.x #int(point.x / self.terrain_np.getScale().x * self.heightmap.getXSize())
@@ -342,7 +527,7 @@ class BaseApp(ShowBase):
         for x in range(int(center_x-max_range),int(center_x+max_range)):
             for y in range(int(center_y-max_range),int(center_y+max_range)):
                 if(x>0 and y>0 and x<self.heightmap.getXSize() and y<self.heightmap.getYSize()):
-                    #TODO: fix this fall off, it works in reverse currently.
+                    #TODO: fix this fall off, it works in reverse currently...
                     # Calculate the distance from the center pixel
                     distance = math.sqrt((x - center_x)**2 + (y - center_y)**2)
                     
@@ -350,7 +535,7 @@ class BaseApp(ShowBase):
                     max_distance = math.sqrt((0 - center_x)**2 + (0 - center_y)**2)  # Max distance to any corner
                     normalized_distance = distance / max_distance
                     current_height = self.heightmap.getGray(x, y)
-                    new_height = min(1.0, current_height + normalized_distance*power)  # Increase height, max 1.0
+                    new_height = min(1.0, current_height + .1*power)  # Increase height, max 1.0
                     self.heightmap.setGray(x, y, new_height)
                 
         
