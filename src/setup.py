25a83179f921ab8bd6ee9680b32a5e158378c604
Cole Hajde <chajde@uccs.edu>
2024-12-01 13:44:49 -0700

    Added setup.py and ran it to create Binaries for Linux, Windoes and MacOS - All within the new build directory

---
+++ src/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="VivariumSim",
+    options={
+        'build_apps':{
+            'gui_apps':{
+                'VivariumSim': 'main.py',
+            },
+            'include_patterns': [
+                '**/*.png',
+                '**/*.jpg',
+                '**/*.egg',
+            ],
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+            ],
+            'platforms': [
+                'manylinux1_x86_64',
+                'macosx_10_6_x86_64',
+                'win_amd64',
+            ],
+        }
+    }
+)
